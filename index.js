a13dc30c6d52b359ab52d16965a9701b8e2a0fdd
EhuDev <jhonardlangs@gmail.com>
2024-04-06 02:50:02 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const displayChar = document.querySelector(".display");
+const animeChars = document.querySelectorAll(".anime-char");
+const timer = document.querySelector("#timer");
+const displayScore = document.querySelector("#score");
+const enterButton = displayChar.nextElementSibling;
+let score = 0;
+let timerInterval;
+
+const inputChar = () => {
+  if (timer.textContent === "Time's up!") {
+    return;
+  }
+  const inputChars = displayChar.value.trim().toLowerCase();
+  let found = false;
+  animeChars.forEach((animeChar) => {
+    const showChars = animeChar.textContent.trim().toLowerCase();
+    if (
+      inputChars === showChars &&
+      animeChar.classList.contains("text-transparent")
+    ) {
+      animeChar.classList.remove("text-transparent");
+      found = true;
+    }
+  });
+  if (found) {
+    score++;
+    displayScore.textContent = `${score}`;
+  }
+  if (!timerInterval) {
+    timerCount();
+  }
+  displayChar.value = "";
+  displayChar.focus();
+};
+
+const timerCount = () => {
+  let count = 60;
+  timerInterval = setInterval(() => {
+    const minutes = Math.floor(count / 60);
+    const seconds = count % 60;
+    timer.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    if (count === 0) {
+      clearInterval(timerInterval);
+      timer.textContent = "Time's up!";
+    } else {
+      count--;
+    }
+  }, 1000);
+};
+
+displayChar.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    enterButton.click();
+    // Start the timer if it hasn't started yet
+    if (!timerInterval) {
+      timerCount();
+    }
+  }
+});
